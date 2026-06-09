86ec840aa4a638fd0199eeb617d92ec40999d145
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2026-06-09 16:37:19 +0530

    feat: add sample seed data for projects and tasks

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/auth", authRoutes);
 
-app.use("/api/v1//projects", projectRoutes);
+app.use("/api/v1/projects", projectRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err);
